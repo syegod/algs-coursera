3e44fe14eec4754688bff0ff1dd1519233592e42
syegod <ovisochin362@gmail.com>
2025-01-08 17:31:03 +0100

    little fix

--- src/test/java/module7/TaxicabNumbers.java
+++ src/test/java/module7/TaxicabNumbers.java
@@ -1,14 +1,14 @@
-package module7;
-
-import java.util.List;
-
-public class TaxicabNumbers {
-    public static List<Integer> findNumbers(int n) {
-
-    }
-
-    private static boolean isTNum(int n) {
-
-    }
-
-}
+//package module7;
+//
+//import java.util.List;
+//
+//public class TaxicabNumbers {
+//    public static List<Integer> findNumbers(int n) {
+//
+//    }
+//
+//    private static boolean isTNum(int n) {
+//
+//    }
+//
+//}
